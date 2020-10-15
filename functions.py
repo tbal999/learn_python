9a634b8f75bc66e20623653f054cb61792b8bfe7
tbal999 <53711814+tbal999@users.noreply.github.com>
2020-10-15 17:00:26 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -60,4 +60,4 @@ if __name__ == "__main__":
 
     print(add5toboth(2,3)) #This will result in a tuple which is a type of data structure (we'll go into them more in 'data structures')
 
-    fibonacci(0, 1, 10, 7) #Calls the fibonacci function.
+    fibonacci(0, 1, 10, 7) #Calls the fibonacci function starting with the numbers 0 and 1, and will print 7 lines of 10 numbers each line.
