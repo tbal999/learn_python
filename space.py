cfbda4ccd2478d4f8238708920eb8b430f190ee5
tbal999 <53711814+tbal999@users.noreply.github.com>
2020-10-15 15:40:51 +0100

    Create space.py

---
+++ space.py
@@ -0,0 +1,24 @@
+# This little script will grab any csv file and remove all whitespace within it.
+# Usage is python script inputfile outputfile.
+# i.e python script.py input.csv output.csv
+
+import pandas as pd #Import pandas - very powerful data maniuplation library
+import sys #sys is a package that has various system interfaces i.e working with arguments in this case
+import os.path #package that enables us to verify whether a file exists.
+
+#process - this function takes in two variables 'inn' and 'out'.
+def process(inn, out):
+    table = pd.read_csv(inn) #Read the csv file 'inn' and place it in a pandas dataframe i've labelled as 'table'.
+    columns = table.columns.values #Grab the columns of the table and place them in a list. 
+    for index, i in enumerate(columns): #Iterate through the columns
+        table[i] = table[i].str.replace(" ","") #For each column we want to iterate through the column and remove all whitespace.
+    table.to_csv(out ,header=True, index=False) #Afterwards we want to save the database as a csv file.
+
+if __name__ == "__main__": #Standard 'entry point' method for python. 
+    if len(sys.argv) <= 1: #If the length of the system arguments is less than or equal to one...
+        exit("usage: python " + sys.argv[0] + " inputfilename outputfilename") #Exit with printing of instructions on how to use this script.
+    else: 
+        if os.path.isfile(sys.argv[1]): #If this file exists
+            process(sys.argv[1], sys.argv[2]) #Invoke process function with arguments 1 and 2 (0 is the script itself!)
+        else:
+            print(sys.argv[1] + " does not exist.") #If file doesn't exist - tell them!
