36dc6884cad18b7688252d228c2b3da785fb571c
tbal999 <53711814+tbal999@users.noreply.github.com>
2020-10-15 20:34:24 +0100

    Update 3_classes.py

--- 3_classes.py
+++ 3_classes.py
@@ -7,8 +7,8 @@ For example, dogs and cats are animals.
 You could, in a limited sense, define an animal.
 
 For this basic example, I'm going to make an animal class.
-When you create the animal, you need to type in a name for it, and a 'greediness' which is an integer.
-Depending on the greediness, it takes so many times to feed the animal before they are happy.
+When you create the animal, you need to type in a name for it, and a 'hunger' which is an integer.
+Depending on the hunger, it takes so many times to feed the animal before they are happy.
 Let's see how this works.
 """
 
@@ -16,14 +16,14 @@ class Animal: #This is the animal class
     #You can pre-define variables to be certain things when it's created
     hungry = True #The animal, by default, will be hungry when you create it.
 
-    def __init__(self, name, greediness): #This is the standard way to 'instantiate' the class when you create it (feed it your own variables to pre-set it when it's created)
-        self.greediness = greediness
+    def __init__(self, name, hunger): #This is the standard way to 'instantiate' the class when you create it (provide it your own variables to pre-set it when it's created)
+        self.hunger = hunger
         self.name = name
 
     #If you want the class to interact with the input of the function, include the 'self' variable. This is a hidden variable that will work within the function.
     def feed(self, food): #In this case, food is an integer. 
-        self.greediness -= food #Deduct the food value from the greediness value
-        if self.greediness < 0: #If greediness is below 0 
+        self.hunger -= food #Deduct the food value from the hunger value
+        if self.hunger < 0: #If hunger is below 0 
             self.hungry = False #Change the hungry variable to false.
 
     def check(self): #Functions within classes are called 'methods'. They're like bolt-ons to a class which it has access to.
