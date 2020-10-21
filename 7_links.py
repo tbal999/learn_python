4fa76963c5bb50c9d03f82b20d7a79b2dd2a752c
tbal999 <53711814+tbal999@users.noreply.github.com>
2020-10-21 12:19:26 +0100

    Create 7_links.py

---
+++ 7_links.py
@@ -0,0 +1,54 @@
+"""
+
+This is just a list of links to other resources so you can continue your learning journey. I've picked various different topics so you can follow your passion.
+
+=== BUILD A GAME ===
+
+1) Build the snake game in python using the turtle package:
+https://www.edureka.co/blog/python-turtle-module/
+
+2) Build the game 'connect 4' in python - part 1 of 4 youtube videos:
+https://www.youtube.com/watch?v=UYgyRArKDEs
+
+
+=== TOOLS ===
+
+1) Build a calculator in python:
+https://www.programiz.com/python-programming/examples/calculator
+
+2) Build an API in python - tonnes of guides (this website also has guides on other things to do in python as well):
+https://realpython.com/tutorials/api/
+
+3) Build a small simple web application in python using the flask package:
+https://medium.com/bhavaniravi/build-your-1st-python-web-app-with-flask-b039d11f101c
+
+4) Build a ceaser cipher (encryption) in python:
+https://www.w3resource.com/python-exercises/string/python-data-type-string-exercise-25.php
+
+5) Build a port scanner in python:
+https://www.pythonforbeginners.com/code-snippets-source-code/port-scanner-in-python
+
+
+=== DATA ANALYSIS / DATA SCIENCE ===
+
+1) A huge number of exercises using pandas (a very popular data analysis library):
+https://www.w3resource.com/python-exercises/pandas/index.php
+
+2) List comprehension - how to use the list data structure in python more efficiently:
+https://www.pythonforbeginners.com/basics/list-comprehensions-in-python
+
+3) How to connect to SQL and perform queries using python pyodbc package alongside pandas:
+https://datatofish.com/how-to-connect-python-to-sql-server-using-pyodbc/
+
+4) Big data analysis - where to begin?
+http://www.data-analysis-in-python.org/t_big_data.html
+
+5) Machine Learning - what is it really?
+https://medium.com/machine-learning-in-practice/cheat-sheet-of-machine-learning-and-python-and-math-cheat-sheets-a4afe4e791b6
+VERY HIGH LEVEL. I'd recommend picking one that you are interested in and learning about it specifically rather than trying to learn everything.
+Key topics include:
+- Neural Networks (there are a LOT of different types of neural network)
+- Regression (Logistical / Linear etc)
+- Clustering algorithms (K nearest neighbours / DBSCAN etc)
+
+"""
